872179070c29b067daf96b92b099d1480f0bd046
Quentin Vilcoque <q.vilcoque@gmail.com>
2017-05-18 14:14:03 +0200

    le role du controleur
    * Manipuler l'objet Request
    - Les paramètres hors routes
    
    * Manipuler l'objet Response
    - Décomposition de la construction d'un objetResponse
    
    (note a moi meme : ne pas mettre "platform" dans l'url
    exemple :
    http://localhost/perso/SymfonyTuto/Symfony/web/app_dev.php/advert/5)

--- Symfony/src/OC/PlatformBundle/Controller/AdvertController.php
+++ Symfony/src/OC/PlatformBundle/Controller/AdvertController.php
@@ -1,41 +1,28 @@
 <?php
 
-/*
-// src/OC/PlatformBundle/Controller/AdvertController.php
-
-namespace OC\PlatformBundle\Controller; // on se place dans le namespace des contrôleurs de notre bundle.
-//Suivez juste la structure des répertoires dans lequel se trouve le contrôleur
-
-use Symfony\Component\HttpFoundation\Response; // notre contrôleur va utiliser l'objet Response,
-//il faut donc le définir grâce au use.
-
-class AdvertController // le nom de notre contrôleur respecte le nom du fichier pour que l'autoload fonctionne.
-{
-    public function indexAction() // on définit la méthode indexAction()
-    {
-        return new Response("Notre propre Hello World !"); // on crée une réponse toute simple.
-        // L'argument de l'objet Response est le contenu de la page que vous envoyez au visiteur
-    }
-}*/
-
-
-
 // src/OC/PlatformBundle/Controller/AdvertController.php
 
 namespace OC\PlatformBundle\Controller;
 
-// N'oubliez pas ce use :
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller
 {
-  public function indexAction()
+  // On modifie viewAction, car elle existe déjà
+  public function viewAction($id)
   {
-    $content = $this
-        ->get('templating')
-        ->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'Quentin'));
+    // On crée la réponse sans lui donner de contenu pour le moment
+    $response = new Response();
 
-    return new Response($content);
+    // On définit le contenu
+    $response->setContent("Ceci est une page d'erreur 404");
+
+    // On définit le code HTTP à « Not Found » (erreur 404)
+    $response->setStatusCode(Response::HTTP_NOT_FOUND);
+
+    // On retourne la réponse
+    return $response;
   }
 }
