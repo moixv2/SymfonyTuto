cbc6ef35a4bf88751e21b2a8351dfbc81feb208c
Quentin Vilcoque <q.vilcoque@gmail.com>
2018-10-06 22:07:19 +0200

    remise à zéro
    - la tout marche car j'ai annulé toute les modif du tuto

--- src/app/appareil-view/appareil-view.component.ts
+++ src/app/appareil-view/appareil-view.component.ts
@@ -1,55 +1,15 @@
-import {Component, OnInit} from '@angular/core';
-import {AppareilService} from '../services/appareil.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appareil-view',
   templateUrl: './appareil-view.component.html',
   styleUrls: ['./appareil-view.component.scss']
 })
-
 export class AppareilViewComponent implements OnInit {
 
-  isAuth = false;
+  constructor() { }
 
-
-  lastUpdate = new Promise(
-    (resolve, reject) => {
-    const date = new Date();
-    setTimeout(
-      () => {
-        resolve(date);
-      }, 2000
-    );
-  });
-
-  appareils: any[];
-
-  constructor(private appareilService: AppareilService ){
-    setTimeout(
-      ()=>{
-        this.isAuth=true;
-
-      }, 4000
-
-    );
-
-  }
-
-  ngOnInit(){
-    this.appareils = this.appareilService.appareils;
-
-  }
-
-  onAllumer(){
-    this.appareilService.switchOnAll();
-  }
-
-  onEteindre(){
-    if(confirm('Etes-vous sûr de vouloir éteindre les appareils ?')){
-      this.appareilService.switchOffAll();
-    } else{
-      return null;
-    }
+  ngOnInit() {
   }
 
 }
